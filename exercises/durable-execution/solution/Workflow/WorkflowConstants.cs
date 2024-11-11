c43c81f41f7eea18c61a7f5c4866cf95486be202
angelazhou32 <angela.zhou@temporal.io>
2024-11-11 10:56:55 -0500

    WorkflowConstants working now

--- exercises/durable-execution/solution/Workflow/WorkflowConstants.cs
+++ exercises/durable-execution/solution/Workflow/WorkflowConstants.cs
@@ -2,5 +2,5 @@ namespace TemporalioDurableExecution;
 
 public static class WorkflowConstants
 {
-    public const string TaskQueueName = "durable-exec-tasks";
+    public static string TaskQueueName => "durable-exec-tasks";
 }
