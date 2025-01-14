898d91479a9044bc9382be9eaf9635b63effbd8c
Alex Garnett <axfelix@gmail.com>
2025-01-13 19:12:52 -0800

    fix some redundant packages and mis-named imports

--- exercises/durable-execution/solution/Workflow/TranslationWorkflow.workflows.cs
+++ exercises/durable-execution/solution/Workflow/TranslationWorkflow.workflows.cs
@@ -3,15 +3,15 @@ namespace TemporalioDurableExecution.Workflow;
 using Microsoft.Extensions.Logging;
 using Temporalio.Workflows;
 
-
 [Workflow]
 public class TranslationWorkflow
 {
     public record TranslationWorkflowInput(string Name, string LanguageCode);
+
     public record TranslationWorkflowOutput(string HelloMessage, string GoodbyeMessage);
 
     [WorkflowRun]
-    public async Task<TranslationWorkflowInput> RunAsync(TranslationWorkflowInput input)
+    public async Task<TranslationWorkflowOutput> RunAsync(TranslationWorkflowInput input)
     {
         var logger = Workflow.Logger;
 
@@ -22,7 +22,7 @@ public class TranslationWorkflow
         logger.LogInformation("Preparing to translate 'Hello', '{LanguageCode}'", input.LanguageCode);
         var helloResult = await Workflow.ExecuteActivityAsync(
            (Activities act) => act.TranslateTermAsync(
-               new TranslateTermInput("hello", input.LanguageCode.ToLower())),
+               new Activities.TranslateTermInput("hello", input.LanguageCode.ToLower())),
            activityOptions);
 
         logger.LogInformation("Sleeping between translation calls");
@@ -31,7 +31,7 @@ public class TranslationWorkflow
         logger.LogInformation("Preparing to translate 'Goodbye', '{LanguageCode}'", input.LanguageCode);
         var goodbyeResult = await Workflow.ExecuteActivityAsync(
             (Activities act) => act.TranslateTermAsync(
-                new TranslateTermInput("goodbye", input.LanguageCode.ToLower())),
+                new Activities.TranslateTermInput("goodbye", input.LanguageCode.ToLower())),
             activityOptions);
 
         return new TranslationWorkflowOutput(
