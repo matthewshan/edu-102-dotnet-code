5f42d7ceee20e57a750e6b6158bddc4f53b42132
angelazhou32 <angela.zhou@temporal.io>
2025-01-21 19:16:01 -0500

    rename for consistency tests

--- exercises/testing-code/practice/Workflow/TranslationWorkflow.workflows.cs
+++ exercises/testing-code/practice/Workflow/TranslationWorkflow.workflows.cs
@@ -31,11 +31,11 @@ public class TranslationWorkflow
         logger.LogInformation("Preparing to translate 'Goodbye', '{LanguageCode}'", input.LanguageCode);
         var goodbyeResult = await Workflow.ExecuteActivityAsync(
             (Activities act) => act.TranslateTermAsync(
-                new Activities.TranslateTermInput("goodbye" input.LanguageCode.ToLower())),
+                new Activities.TranslateTermInput("goodbye", input.LanguageCode.ToLower())),
             activityOptions);
 
         return new TranslationWorkflowOutput(
             $"{helloResult.Translation}, {input.Name}",
-            $"{goodbyeResult.Translation}, {input.Name}");
+            $"{goodbyeResult.Translation} {input.Name}");
     }
 }
